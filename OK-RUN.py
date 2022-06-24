d28f334a7d3f65c1676f1145fbe3eaee730d2d1c
SecDet <96675285+C4LLM3D3V1L@users.noreply.github.com>
2022-06-24 21:27:09 +0600

    Update OK-RUN.py

--- OK-RUN.py
+++ OK-RUN.py
@@ -4,8 +4,8 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from WOW import menu
+    from oldclone import menu
     menu()
 elif bit == '32bit':
-    from WOW import menu
+    from oldclone import menu
     menu()
