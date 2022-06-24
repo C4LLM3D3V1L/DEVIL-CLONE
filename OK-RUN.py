dd031c862a033ba9bd54eda57404f7a3c92cc56d
SecDet <96675285+C4LLM3D3V1L@users.noreply.github.com>
2022-06-24 21:14:24 +0600

    Update OK-RUN.py

--- OK-RUN.py
+++ OK-RUN.py
@@ -1,1 +1,11 @@
-import WOW
+import os, platform
+os.system('git pull')
+ 
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from WOW import menu
+    menu()
+elif bit == '32bit':
+    from WOW import menu
+    menu()
