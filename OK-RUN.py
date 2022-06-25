58c47cba8e53158d3d69bc36063b459a637cac00
SecDet <96675285+C4LLM3D3V1L@users.noreply.github.com>
2022-06-25 09:54:02 +0600

    Update OK-RUN.py

--- OK-RUN.py
+++ OK-RUN.py
@@ -4,8 +4,8 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from oldclone import menu
+    from old import menu
     menu()
 elif bit == '32bit':
-    from oldclone import menu
+    from old import menu
     menu()
